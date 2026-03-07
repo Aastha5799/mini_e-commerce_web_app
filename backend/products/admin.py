1b5123619e8102de8fbf378cce46a58ad337f0e1
astha <aasthamainali5@gmail.com>
2026-03-07 12:07:10 +0545

    Complete project with JWT authentication

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Product
-from pathlib import Path
+from .models import Product, CustomerUser, Cart, Order, OrderItem
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL = '/media/'               # URL to access files
-MEDIA_ROOT = BASE_DIR / 'media'
 admin.site.register(Product)
+admin.site.register(CustomerUser)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
